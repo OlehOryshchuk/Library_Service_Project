12db10f2499d18dd00aff2f15c3953f0faf37dd5
Oleh Oryshchuk <olehoryshshuk@gmail.com>
2023-12-09 21:40:54 +0200

    refact: Remove duplicated code

--- library_service/urls.py
+++ library_service/urls.py
@@ -21,5 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("books.urls", namespace="book")),
-    path("", include("user.urls", namespace="user")),
+    path("users/", include("user.urls", namespace="user")),
 ]
--- user/urls.py
+++ user/urls.py
@@ -8,12 +8,12 @@ from rest_framework_simplejwt.views import (
 from .views import CreateUserView, ManagerUserView
 
 urlpatterns = [
-    path("users/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("users/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    path("users/token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
 
-    path("users/register/", CreateUserView.as_view(), name="create"),
-    path("users/me/", ManagerUserView.as_view(), name="manage"),
+    path("register/", CreateUserView.as_view(), name="create"),
+    path("me/", ManagerUserView.as_view(), name="manage"),
 ]
 
 app_name = "user"
