5fcd080edc2b535daf8f8d85047dfc30628befaa
Oleh Oryshchuk <olehoryshshuk@gmail.com>
2023-12-09 17:38:45 +0200

    feat: Create model Book and migrate it

--- books/models.py
+++ books/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    class CoverType(models.TextChoices):
+        HARD = "HARD", "Hardcover"
+        SOFT = "SOFT", "Softcover"
+
+    title = models.CharField(max_length=100, unique=True)
+    author = models.CharField(max_length=255)
+    cover = models.CharField(
+        choices=CoverType.choices,
+        max_length=10,
+        blank=True
+    )
+    inventory = models.PositiveIntegerField()
+    daily_fee = models.DecimalField(decimal_places=4, max_digits=20)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=["title"]),
+            models.Index(fields=["author"]),
+        ]
