e7b2170d65b9b9fa253c373837b9ba2cd5c24973
Oleh Oryshchuk <olehoryshshuk@gmail.com>
2023-12-09 21:03:34 +0200

    feat: Create UserManageView with JWTAuthentication and with permission class IsAuthenticated

--- user/views.py
+++ user/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
-# Create your views here.
+from .serializers import UserSerializer
+
+
+class UserManageView(
+    generics.RetrieveUpdateAPIView,
+    generics.CreateAPIView
+):
+    serializer_class = UserSerializer
+    authentication_classes = (JWTAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self):
+        return self.request.user
