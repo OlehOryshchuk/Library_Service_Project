b4f3518ec084c4361572dd86644d1518dbb31159
Oleh Oryshchuk <olehoryshshuk@gmail.com>
2023-12-11 19:10:14 +0200

    feat: Create BorrowingCreateSerializer

--- borrowings/serializers.py
+++ borrowings/serializers.py
@@ -1,7 +1,30 @@
 from rest_framework.serializers import ModelSerializer
+from django.core.exceptions import ValidationError
 
 from books.serializers import BookDetailSerializer
 from .models import Borrowing
+
+
+class BorrowingCreateSerializer(ModelSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        Borrowing.validate_dates(
+            attrs["actual_return_date"],
+            attrs["expected_return_date"],
+            ValidationError
+        )
+
+
+    class Meta:
+        model = Borrowing
+        fields = [
+            "borrow_date",
+            "expected_return_date",
+            "actual_return_date",
+            "book",
+        ]
+        read_only_fields = ["borrow_date"]
 
 
 class BorrowingListSerializer(ModelSerializer):
