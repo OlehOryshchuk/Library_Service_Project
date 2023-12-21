6d6d772b7bb21a4344405a8fc10c15aaa9a355d1
Oleh Oryshchuk <olehoryshshuk@gmail.com>
2023-12-21 20:04:34 +0200

    refact: Creation of Stripe Checkout Session through StripeSessionCreator with Borrowing helpers methods, making it more modular and maintainable

--- payments/stripe_api.py
+++ payments/stripe_api.py
@@ -1,3 +1,5 @@
+from typing_extensions import Literal
+
 import stripe
 from django.conf import settings
 from django.db import transaction
@@ -7,74 +9,64 @@ from payments.models import Payment
 from borrowings.models import Borrowing
 
 
-def borrowing_days(borrowing: Borrowing) -> int:
-    """Return number of days the book will be borrowed"""
-    return (
-            borrowing.expected_return_date -
-            borrowing.borrow_date
-    ).days
+class StripeSessionCreator:
+    def __init__(self, borrowing: Borrowing, payment_type: Literal["PAYMENT", "FINE"] = "PAYMENT"):
+        self.borrowing = borrowing
+        self.payment_type = payment_type
 
+    def _get_price(self) -> int:
+        if self.payment_type == "FINE":
+            return int(self.borrowing.fee_price() * 100)
+        else:
+            return int(self.borrowing.price_for_borrowing() * 100)
 
-def total_price(borrowing: Borrowing) -> int:
-    """
-    Calculate total price for borrowing
-    """
-    return borrowing_days(borrowing) * borrowing.book.daily_fee
+    def _get_message(self) -> str:
+        if self.payment_type == "FINE":
+            return f"You are paying for overdue borrowing days - {self.borrowing.num_of_overdue_days()}"
+        else:
+            return f"You are paying for borrowing time of book {self.borrowing.book.title} - {self.borrowing.book.author}"
 
+    @transaction.atomic
+    def create_checkout_session(self, request) -> str:
+        stripe.api_key = settings.STRIPE_API_KEY
 
-def price_in_cents(borrowing: Borrowing) -> int:
-    return int(total_price(borrowing=borrowing) * 100)
+        book = self.borrowing.book
+        price = self._get_price()
 
+        payment = Payment.objects.create(
+            status="PENDING",
+            type=self.payment_type,
+            borrowings=self.borrowing,
+            money_to_pay=price,
+        )
 
-@transaction.atomic
-def create_payment_session(borrowing: Borrowing, request) -> None:
-    """
-    Create Stripe Payment Checkout Session to pay for borrowing
-    and create Payment record using Session.url and id
-    finally return url to Stripe-hosted payment page.
-    """
-    stripe.api_key = settings.STRIPE_API_KEY
-
-    book = borrowing.book
-    payment = Payment.objects.create(
-        status="PENDING",
-        type="PAYMENT",
-        borrowings=borrowing,
-        money_to_pay=total_price(borrowing),
-    )
-
-    checkout_session = stripe.checkout.Session.create(
-        line_items=[
-            {
-                "price_data": {
-                    "currency": "usd",
-                    "unit_amount": price_in_cents(borrowing),
-                    "product_data": {
-                        "name": f"{book.author} - {book.title}"
+        checkout_session = stripe.checkout.Session.create(
+            line_items=[
+                {
+                    "price_data": {
+                        "currency": "usd",
+                        "unit_amount": price,
+                        "product_data": {
+                            "name": f"{book.author} - {book.title}"
+                        },
                     },
-                },
-                "quantity": 1,
+                    "quantity": 1,
+                }
+            ],
+            mode="payment",
+            success_url=request.build_absolute_uri(
+                reverse("payments:payment-success", args=[payment.id])
+            ),
+            cancel_url=request.build_absolute_uri(
+                reverse("payments:payment-cancel", args=[payment.id])
+            ),
+            custom_text={
+                "submit": {"message": self._get_message()}
             }
-        ],
-        mode="payment",
-        success_url=request.build_absolute_uri(
-            reverse("payments:payment-success", args=[payment.id])
-        ),
-        cancel_url=request.build_absolute_uri(
-            reverse("payments:payment-cancel", args=[payment.id])
-        ),
-        custom_text={
-            "submit": {"message": (
-                "You will pay for borrowing time - "
-                f"{borrowing_days(borrowing)} days"
-            )
-            }
-        }
-    )
+        )
 
-    payment.session_id = checkout_session.id
-    payment.session_url = checkout_session.url
+        payment.session_id = checkout_session.id
+        payment.session_url = checkout_session.url
+        payment.save()
 
-    payment.save()
-
-    return checkout_session.url
+        return checkout_session.url
