1ca51029483622484daa35a272fe14d9f46e428b
Oleh Oryshchuk <olehoryshshuk@gmail.com>
2023-12-23 22:06:03 +0200

    refact: Add borrowing_book field in Payment Serializers and using BookDetailSerializer for serializing borrowed book

--- payments/serializers.py
+++ payments/serializers.py
@@ -1,32 +1,41 @@
 from rest_framework import serializers
 
-from django.utils import timezone
-
-from borrowings.serializers import BorrowingDetailSerializer
+from books.serializers import BookDetailSerializer
 
 from .models import Payment
 
 
 class PaymentListSerializer(serializers.ModelSerializer):
-    borrowing = BorrowingDetailSerializer(read_only=True)
+    borrowing_book = BookDetailSerializer(
+        read_only=True,
+        source="borrowing.book"
+    )
 
     class Meta:
         model = Payment
         fields = [
             "id",
             "status",
+            "type",
+            "borrowing_book",
             "borrowing",
         ]
 
 
 class PaymentDetailSerializer(serializers.ModelSerializer):
-    borrowing = BorrowingDetailSerializer(read_only=True)
+    borrowing_book = BookDetailSerializer(
+        read_only=True,
+        source="borrowing.book"
+    )
 
     class Meta:
+
         model = Payment
         fields = [
             "id",
             "status",
+            "type",
+            "borrowing_book",
             "borrowing",
             "money_to_pay",
             "session_url",
