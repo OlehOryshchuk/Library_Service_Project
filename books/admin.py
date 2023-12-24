aa3d836e282b2848f7a0ff10d51e3c869597ed92
Oleh Oryshchuk <olehoryshshuk@gmail.com>
2023-12-24 17:48:55 +0200

    feat: Register Models on Django admin site

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,7 @@
+from django.contrib.auth.models import Group
 from django.contrib import admin
 
-# Register your models here.
+from .models import Book
+
+admin.site.unregister(Group)
+admin.site.register(Book)
