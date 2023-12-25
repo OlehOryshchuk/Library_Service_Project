94de639dc1809b874c3ed6db24c339811c4781d4
Oleh Oryshchuk <olehoryshshuk@gmail.com>
2023-12-25 16:00:01 +0200

    refact: Change borrowing_sample test utils to create payment session for every Borrowing

--- test_utils/borrowing_samples.py
+++ test_utils/borrowing_samples.py
@@ -5,8 +5,10 @@ from django.contrib.auth import get_user_model
 from borrowings.models import Borrowing
 from books.models import Book
 
+from payments.stripe_api import StripeSessionHandler
 
-def borrowing_sample(book: Book, user: get_user_model, **param):
+
+def borrowing_sample(book: Book, user: get_user_model, request, **param):
     """Function for tests, return borrowing instance"""
     default = {
         "expected_return_date":  date.today() + timedelta(days=4),
@@ -14,6 +16,13 @@ def borrowing_sample(book: Book, user: get_user_model, **param):
         "book": book,
         "user": user,
     }
+    # Borrowing list, detail endpoints use some fields
+    # from borrowing payment, so we need to create payment
+
     default.update(**param)
 
-    return Borrowing.objects.create(**default)
+    borrowing = Borrowing.objects.create(**default)
+    stripe_session = StripeSessionHandler(borrowing)
+    stripe_session.create_checkout_session(request=request)
+
+    return borrowing
